3334f210ae5c77fc230193e85535b24ca8800c5e
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-26 16:07:53 +0200

    Added a directive for the image

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var app = require('./config/express');
 var db = require('./config/database');
 
 http.createServer(app).listen(3000, function() {
-    console.log('Servidor estutando na porta: ' + this.address().port);
+    console.log('Servidor escutando na porta: ' + this.address().port);
 });
